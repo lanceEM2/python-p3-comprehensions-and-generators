25c53d098a1bc451bb90fd0bc711f0b545a47f4f
Dylan <dylanlancekemboi@gmail.com>
2023-12-09 08:57:23 +0300

    Completed Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_num = [n for n in num_list if n % 2 == 0]
+    return even_num
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation = [word + '!' for word in sentence_list]
+    return exclamation
